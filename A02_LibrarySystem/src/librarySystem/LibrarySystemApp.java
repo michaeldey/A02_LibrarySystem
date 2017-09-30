28c4cbf05686b435b54e06a200d9e76b3ef79c26
michaeldey <michaeldey@hotmail.com>
2017-09-30 17:44:35 -0600

    Refined table creation code, optimized the DatabaseControl constructor to eliminate errors on table creation of existing tables.  Now the DatabaseControl object can be created without errors.  Refined book object and patron object addition methods.

--- A02_LibrarySystem/src/librarySystem/LibrarySystemApp.java
+++ A02_LibrarySystem/src/librarySystem/LibrarySystemApp.java
@@ -6,12 +6,22 @@ public class LibrarySystemApp {
 
 	public static void main(String[] args) throws SQLException {
 		DatabaseControl db = new DatabaseControl();
-		//db.createBooksTable();
-		//db.createPatronsTable();
-		//db.addBook();
-		//db.addPatron();
+		
+//		db.addBook("The Art of War", "Sun Tsu", "Self Help");
+//		db.addBook("Siddhartha", "Herman Hesse", "Literature");
+//		db.addBook("Hop On Pop", "Dr. Seusse", "Kids");
+//		db.addBook("Green Eggs and Ham", "Dr. Seusse", "Kids");
+//		db.addBook("Cat In The Hat", "Dr. Seusse", "Kids");
+//		db.addBook("The Art of Peace", "Morihei Ueshiba", "Self Help");		
+//		
 		db.showAllFromQuery("SELECT * FROM Books");
-		System.out.println();
+//		db.showAllFromQuery("SELECT * FROM Books WHERE GENRE='Kids' ORDER BY Title");
+//		
+//		System.out.println();
+//		
+//
+//		db.addPatron("Mike", "Dey");
+//		db.addPatron("John", "Smith");
 		db.showAllFromQuery("SELECT * FROM Patrons");
 	}
 
